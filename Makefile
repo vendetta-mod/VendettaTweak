baa04e397741c7f03719a60d989b5d63cc0bdead
Jack Matthews <jm5112356@gmail.com>
2023-01-08 02:48:42 -0500

    Initial commit

---
+++ Makefile
@@ -0,0 +1,12 @@
+TARGET := iphone:clang:latest:14.0
+
+
+include $(THEOS)/makefiles/common.mk
+
+TWEAK_NAME = VendettaTweak
+
+VendettaTweak_FILES = $(shell find Sources/VendettaTweak -name '*.swift') $(shell find Sources/VendettaTweakC -name '*.m' -o -name '*.c' -o -name '*.mm' -o -name '*.cpp')
+VendettaTweak_SWIFTFLAGS = -ISources/VendettaTweakC/include
+VendettaTweak_CFLAGS = -fobjc-arc -ISources/VendettaTweakC/include
+
+include $(THEOS_MAKE_PATH)/tweak.mk
---
+++ Sources/VendettaTweak/Tweak.x.swift
@@ -0,0 +1,32 @@
+import Foundation
+import Orion
+import VendettaTweakC
+
+class LoadVendettaHook: ClassHook<NSObject> {
+  static var targetName = "RCTCxxBridge"
+
+  func executeApplicationScript(_ script: Data, url: URL, async: Bool) {
+    // Fake URL that the modules patch and Vendetta came from
+    let source = URL(string: "vendetta")!
+
+    // Load modules patch
+    let modulesPatch =
+      "const oldObjectCreate = this.Object.create; const win = this; win.Object.create = (...args) => { const obj = oldObjectCreate.apply(win.Object, args); if (args[0] === null) { win.modules = obj; win.Object.create = oldObjectCreate; } return obj; };"
+    let modulesPatchData = modulesPatch.data(using: .utf8)!
+    orig.executeApplicationScript(modulesPatchData, url: source, async: false)
+
+    // Load Discord
+    orig.executeApplicationScript(script, url: url, async: async)
+
+    // Vendetta URL and URLRequest
+    let releaseUrl = URL(
+      string: "https://raw.githubusercontent.com/vendetta-mod/builds/master/vendetta.js")!
+    let request = URLRequest(url: releaseUrl, cachePolicy: .reloadIgnoringCacheData)
+
+    // Try to load Vendetta
+    do {
+      let vendetta = try NSURLConnection.sendSynchronousRequest(request, returning: nil)
+      orig.executeApplicationScript(vendetta, url: source, async: async)
+    } catch {}
+  }
+}
